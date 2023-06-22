d5b547bff35816368c3e4e86776e7b0453e821ea
zatiez <nuruzzati000@gmail.com>
2023-06-22 07:12:48 +0700

    upload first

---
+++ popUp.js
@@ -0,0 +1,10 @@
+alert('aku ada sesuatu nihh buat kamu');
+alert('btw aku mau nanya dulu');
+var a = prompt('nama kamu siapa');
+var b = prompt('kalo umur kamu berapa, isi pake angka aja ya');
+
+alert('haloooo tuan putri ' + a + ' yang cantik lucu nan imut, oh umur kamu ' + b + ' tahun ya?? aku tau kita emang ngga seumuran, tapi boleh kali ya kalo aku bisa seumur hidup sama kamu HAAHAHAHHAHAH');
+alert('eh eh eh uda lama???');
+alert('cantiknya HAHAHAHAHAHHA');
+alert('bosen ya??');
+alert('cantik teruss');
